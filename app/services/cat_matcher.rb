cb1deee577963f2c8b2012deeb713e75badee017
musclecarhags <50854246+musclecarhags@users.noreply.github.com>
2019-07-10 23:42:11 -0400

    Add files via upload

--- app/services/cat_matcher.rb
+++ app/services/cat_matcher.rb
@@ -23,10 +23,9 @@ class CatMatcher
   def matching_cat? cat
     tags = cat['tags'].map(&:downcase)
     environment = cat['environment']
-    age = []
     age = cat['age'].downcase
 
-    if matching_ages.include? age
+    if matching_ages.any? age
       matched_age = true
     else
       matched_age = false
@@ -37,24 +36,22 @@ class CatMatcher
     else
       status_matched = true
     end
-    # for each keyword, checking if the tag is included in the keyword
 
+    # for each keyword, checking if the tag is included in the keyword
     matched_tags = tags.select do |tag|
       final_keywords.any? {|k| tag.include? k}
     end
-
     match_found = matched_tags.length > 0
 
+    # Calculate percent match
     cat['percent'] = matched_tags.length.to_f / tags.length.to_f * 100
-
-    exclude = false
 
     # Get the intersection of tags and exclusion keywords.
     # If there are matched exclusions, this cannot be a match.
+    exclude = false
     excluded_tags = tags.select do |tag|
-      exclusion_keywords.any? {|k| tag.include? k}
+      exclusion_keywords.any? { |k| tag.include? k }
     end
-
     if excluded_tags.length > 0
       exclude = true
     end
@@ -70,54 +67,56 @@ class CatMatcher
     if matching_environment.include? 'cats'
       environment_matched &= cat['environment']['cats']
     end
+
     match_found && environment_matched && matched_age && status_matched && !exclude
   end
 
-def quiz_answers
-    @quiz_answers ||= quiz_questions
-      .zip( answers )
-      .collect { |q,a| q }
-end
-
-def yes_answers
-  @yes_answers ||= quiz_questions
-    .zip( answers )
-    .select { |q,a| a == "yes" }
-    .collect { |q,a| q }
+  def quiz_answers
+      @quiz_answers ||= quiz_questions
+        .zip( answers )
+        .collect { |q,a| a }
   end
 
-def matching_environment
-  @matching_environment ||= yes_answers
-    .collect { |q| (q[:environment] || []).map(&:downcase)}.flatten.compact || [] & ages
-end
+  def yes_questions
+    @yes_questions ||= quiz_questions
+      .zip( answers )
+      .select { |q,a| a == "yes" }
+      .collect { |q,a| q }
+    end
 
-def matching_keywords
-  @matching_keywords ||= yes_answers
-    .collect { |q| (q[:keywords] || []).map(&:downcase)}.flatten.compact || []
-end
+  def matching_environment
+    @matching_environment ||= yes_questions
+      .collect { |q| (q[:environment] || []).map(&:downcase)}.flatten.compact || []
+  end
 
-def matching_ages
-  @matching_ages ||= quiz_answers & ['baby', 'young', 'adult', 'senior']
-end
+  def matching_keywords
+    @matching_keywords ||= yes_questions
+      .collect { |q| (q[:keywords] || []).map(&:downcase)}.flatten.compact || []
+  end
 
-def exclusion_keywords
-  @exclusion_keywords ||= yes_answers
-    .collect { |q| (q[:exclusions] || []).map(&:downcase)}.flatten.compact || []
-end
+  def matching_ages
+    @matching_ages ||= quiz_answers & ['baby', 'young', 'adult', 'senior']
+  end
 
-def quiz_questions
-  @quiz_questions ||= Quiz.questions
-end
+  def exclusion_keywords
+    @exclusion_keywords ||= yes_questions
+      .collect { |q| (q[:exclusions] || []).map(&:downcase)}.flatten.compact || []
+  end
 
-def adoptable_cats
-  @adoptable_cats ||= CatFinder.animals
-end
+  def adoptable_cats
+    @adoptable_cats ||= CatFinder.animals
+  end
 
-def final_keywords
-  @final_keywords ||= matching_keywords - keyword_intersection
-end
+  def final_keywords
+    @final_keywords ||= matching_keywords - keyword_intersection
+  end
 
-def keyword_intersection
-  @keyword_intersection = matching_keywords & exclusion_keywords
-end
+  def keyword_intersection
+    @keyword_intersection = matching_keywords & exclusion_keywords
+  end
+
+  def quiz_questions
+    @quiz_questions ||= Quiz.questions
+  end
+
 end
